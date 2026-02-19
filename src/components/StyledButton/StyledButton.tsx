6d5139a0a1bc0a3c7f40b9a2526a94f728770f79
Adriano Santos <adrianofrontend@gmail.com>
2026-02-19 19:33:00 -0300

    feat: creates StyledButton Component

---
+++ src/components/StyledButton/StyledButton.tsx
@@ -0,0 +1,34 @@
+import { styled } from '@mui/material/styles';
+
+const StyledButtonRoot = styled('button')(({ theme }) => ({
+  backgroundColor: theme.palette.primary.main,
+  color: theme.palette.primary.contrastText,
+  border: `1px solid ${theme.palette.primary.contrastText}`,
+  padding: '5px 15px',
+  borderRadius: '3px',
+  width: '100%',
+  cursor: 'pointer',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  gap: '8px',
+  '&:hover': {
+    backgroundColor: theme.palette.secondary.light,
+  },
+}));
+
+type StyledButtonProps = {
+  children: React.ReactNode;
+  startIcon?: React.ReactNode;
+};
+
+const StyledButton = ({ children, startIcon }: StyledButtonProps) => {
+  return (
+    <StyledButtonRoot>
+      {startIcon}
+      {children}
+    </StyledButtonRoot>
+  );
+};
+
+export default StyledButton;
